f07bb616bd3c38d55fb8310ceb9923be6a1c8ee6
Dmitry Shamrikov <bivis08@gmail.com>
2013-10-29 22:33:24 +0600

    fuck... test project has been killed

---
+++ GameServer/Tests/CollisionResolverMasterTest.cpp
@@ -0,0 +1,43 @@
+#include "CppUnitTest.h"
+#include "../GameServer/world/collisions/collisionresolvermaster.h"
+#include "../GameServer/world/robot/robot.h"
+#include "../GameServer/exceptions.h"
+#include "../GameServer/world/collisions/exceptions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(CollisionResolverMasterTest)
+	{
+	public:
+		class NewMapElement : public MapElement {
+		public:
+			NewMapElement() : MapElement(0, 0, 0, 0) {}
+			virtual std::string GetType() { return "NewMapElement"; }
+		};
+
+		TEST_METHOD(TestResolve)
+		{
+			RobotFrame robotFrame1(10, 10);
+			RobotFrame robotFrame2(20, 20);
+
+			try {
+				CollisionResolverMaster::Resolve(&robotFrame1, &robotFrame2);
+			}
+			catch (NotImplementedException) {}
+		}
+
+		TEST_METHOD(TestResolveWithCollisionResolverNotFoundException)
+		{
+			NewMapElement newMapElement1;
+			NewMapElement newMapElement2;
+
+			try {
+				CollisionResolverMaster::Resolve(&newMapElement1, &newMapElement2);
+				throw 1;
+			}
+			catch (CollisionResolverNotFoundException) {}
+		}
+	};
+}
